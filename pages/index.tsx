005238884a239715737b7dbaf616556bd2e39502
sacheenShyama <nomadsachin2002@gmail.com>
2024-03-11 12:03:12 +0530

    new update

---
+++ pages/index.tsx
@@ -0,0 +1,105 @@
+import Head from 'next/head'
+import { modalState } from '../atoms/modelAtom'
+import { useRecoilValue } from 'recoil'
+
+import Banner from '../components/Banner'
+import Headers from '../components/Headers'
+import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
+import { Movie } from '../typings'
+import Model from '../components/Model'
+import requests from '../utils/requests'
+
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
+  const { logout, loading } = useAuth()
+  const showModel = useRecoilValue(modalState)
+
+  if (loading) {
+    return null
+  }
+
+  return (
+    <div
+      className="relative h-screen bg-gradient-to-b 
+     lg:h-[140vh]"
+    >
+      <Head>
+        <title>Home netflix-clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Headers />
+      <main className="relative pl-4 pb-24 scrollbar-hide lg:space-y-24 lg:pl-16 ">
+        <Banner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
+      </main>
+      {showModel && <Model />}
+    </div>
+  )
+}
+export default Home
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+}
