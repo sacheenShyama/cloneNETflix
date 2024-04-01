e6f4374adcb7aacaaf82b4f34f5c6eedfc302efe
sacheenShyama <nomadsachin2002@gmail.com>
2024-04-01 10:40:33 +0530

    new

--- pages/login.tsx
+++ pages/login.tsx
@@ -31,7 +31,7 @@ function login() {
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
-        <title>Netflix</title>
+        <title>Netflix clone</title>
         <link rel="icon" href="/favicon.icon" />
       </Head>
       <Image
@@ -55,7 +55,7 @@ function login() {
         className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
       >
         <div className="space-y-4">
-          <h1 className="text-4xl font-semibold">Sign In</h1>
+          <h1 className="text-4xl font-semibold">Netflix clone</h1>
           <label htmlFor="" className="inline-block w-full">
             <input
               type="email"
@@ -93,10 +93,10 @@ function login() {
         </button>
 
         <div className="text-[gray]">
-          New to Netflix?{' '}
+          New to Netflix? fill-up details and click on --
           <button
             type="submit"
-            className="text-white hover:underline"
+            className="text-[#e50914] hover:underline font-bold"
             onClick={() => setLogin(false)}
           >
             Sign Up
